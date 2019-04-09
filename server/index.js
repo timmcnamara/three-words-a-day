a87edfd5556b52de51aabc4895d784e35295a0d0
timmcnamara <timlmcn@gmail.com>
2019-04-09 23:27:47 +1000

    updated API calls server side

--- server/index.js
+++ server/index.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
+const axios = require("axios");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/words", (req, res) => {
-  // add api call to oxford here..
-  const name = req.query.name || "World";
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  const { word } = req.query;
+  const { OXFORD_API_USER, OXFORD_API_KEY } = dotenv.parsed;
+
+  axios
+    .get(
+      `https://od-api.oxforddictionaries.com/api/v1/entries/en/${word}/sentences`,
+      {
+        headers: {
+          app_id: `${OXFORD_API_USER}`,
+          app_key: `${OXFORD_API_KEY}`
+        }
+      }
+    )
+    .then(response => {
+      res.setHeader("Content-Type", "application/json");
+      res.json(response.data);
+    })
+    .catch(err => console.log("**", err));
 });
 
 app.listen(3001, () =>
--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Button from "./Button";
 import TextInput from "./TextInput";
+import axios from "axios";
 
 class SearchContainer extends React.Component {
   handleClick(e) {
     e.preventDefault();
+    // make call to api
+    // send a param along to request the word
     console.log("Submitting....");
+    axios
+      .get("http://localhost:3001/api/words", {
+        params: {
+          word: "car"
+        }
+      })
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
   }
 
   onChange(e) {
